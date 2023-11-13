73c0fbc384fdd60fac77221ccee1f4c4ccbb8d79
SharmilaAnanthasayanam <112562560+SharmilaAnanthasayanam@users.noreply.github.com>
2023-11-13 12:24:22 +0530

    Stack Implementation

---
+++ Stack/stack.py
@@ -0,0 +1,19 @@
+class Stack:
+  def __init__(self):
+    self.value = []
+    self.limit = 10
+    
+  def push(self,val):
+    if len(self.value)< self.limit:
+      self.value.append(val)
+    else:
+      print("Stack Overflow")
+  
+  def pop(self):
+    if len(self.value)>0:
+      self.value.pop()
+    else:
+      print("Stack Underflow")
+      
+  def display(self):
+    print(self.value)
